06019659c4fca3324d2e55f7717d8a323fbdba38
rizki <rizki571@gmail.com>
2024-07-13 04:21:48 +0700

    googletag

--- src/app/Head.tsx
+++ src/app/Head.tsx
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
 
+import Script from "next/script";
+
 export const metadata = {
   title: "Sunik Johan",
   description:
@@ -82,10 +84,34 @@ const Head = () => {
       <link rel="icon" href={faviconUrl} />
       <link rel="apple-touch-icon" href={faviconUrl} />
       <link rel="shortcut icon" href={faviconUrl} type="image/x-icon" />
+
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TPGTC838');
+            `,
+        }}
+      />
+
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: jsonLdString }}
       />
+
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TPGTC838"
+          height="0"
+          width="0"
+          title="Google Tag Manager"
+          style={{ display: "none", visibility: "hidden" }}></iframe>
+      </noscript>
     </Fragment>
   );
 };
